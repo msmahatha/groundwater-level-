438e6352067595b3e272112d37af0701763f8eb0
Madhusudan Mahatha <128399085+msmahatha@users.noreply.github.com>
2025-08-08 14:51:36 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -99,3 +99,6 @@ def get_all_cities_data():
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001, debug=True)
 
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5001, debug=True)
+
